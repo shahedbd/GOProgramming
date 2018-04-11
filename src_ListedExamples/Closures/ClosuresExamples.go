4f25a1286f6113d572774c666e59aa4309882f26
shahed-mlbd <sahidul.islam@sekai-lab-bd.com>
2018-04-11 09:52:56 +0600

    + Closer examples added

---
+++ src_ListedExamples/Closures/ClosuresExamples.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
+
+//Go supports anonymous functions, which can form closures. Anonymous functions are useful when you want to define a function inline without having to name it.
